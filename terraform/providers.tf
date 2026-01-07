1233f6e277cdd5277d71d6ad4f48f7c59214b0d7
Zaheer <hzaheer130@gmail.com>
2026-01-07 18:24:18 +0530

    updated files

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,8 @@
-terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
